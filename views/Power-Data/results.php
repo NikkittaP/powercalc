08fa96e7746483138ac52ed470033cb53c59ecb7
Nikita Petrov <nikitapetroff@gmail.com>
2018-04-23 14:31:56 +0300

    Вынесены в отдельный файл Nвых по частям аппарата.

--- views/Power-Data/results.php
+++ views/Power-Data/results.php
@@ -43,39 +43,12 @@ $this->params['breadcrumbs'][] = $this->title;
 <br />
 <div class="row">
     <div class="col-sm-6">
-    <table class="table table-bordered table-condensed table-hover medium kv-table">
-        <tbody>
-        <tr class="active">
-            <th rowspan="2"></th>
-            <th colspan="<?=count($flightModeModel);?>" class="text-center"><h4><b>Nвых</b></h4></th>
-        </tr>
-        <tr class="active">
-            <?php
-            foreach ($flightModeModel as $currentFlightMode) {
-            ?>
-            <th class="text-center"><?= $currentFlightMode->name;?></th>
-            <?php
-            }
-            ?>
-        </tr>
-        <?php
-        foreach ($aircraftPartsModel as $currentAircraftPart) {
+        <?= $this->render('_results_by_aircraft_parts', [
+                'flightModeModel' => $flightModeModel,
+                'aircraftPartsModel' => $aircraftPartsModel,
+                'N_out_by_parts' => $N_out_by_parts,
+                ]);
         ?>
-        <tr>
-            <td><b><?= $currentAircraftPart->name;?></b></td>
-            <?php
-            foreach ($flightModeModel as $currentFlightMode) {
-            ?>
-            <td><?= (isset($N_out_by_parts[$currentFlightMode->id][$currentAircraftPart->id])) ? $N_out_by_parts[$currentFlightMode->id][$currentAircraftPart->id] : '&ndash;';?></td>
-            <?php
-            }
-            ?>
-        </tr>
-        <?php
-        }
-        ?>
-        </tbody>
-    </table>
     </div>
 </div>
 <br />
