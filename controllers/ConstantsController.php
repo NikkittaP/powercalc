596cdad01c9cbe0efbd4bdb6de3418c5ef95137e
Nikita Petrov <nikitapetroff@gmail.com>
2018-08-14 16:27:41 +0300

    2.	Использовать интерполяцию значений КПД насосов по-умолчанию (КПДfixed=0).

--- controllers/ConstantsController.php
+++ controllers/ConstantsController.php
@@ -61,7 +61,7 @@ class ConstantsController extends Controller
     public function actionLoaddefaults()
     {
         $defaultData = [
-            ['isEfficiencyFixed', '1', 'КПД fix?'],
+            ['isEfficiencyFixed', '0', 'КПД fix?'],
             ['chartWidth', '1450', 'Ширина графика'],
             ['chartHeight', '850', 'Высота графика'],
             ['defaultChartColors', '#2f7ed8,#0d233a,#8bbc21,#910000,#1aadce,#492970,#f28f43,#77a1e5,#c42525,#a6c96a', 'Список стандартных цветов для графиков архитектур'],
